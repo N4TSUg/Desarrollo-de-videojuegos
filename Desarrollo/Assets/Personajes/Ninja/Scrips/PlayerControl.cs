144e7ddd8777b28ffa69cef2fc7d2c1dcf424d04
N4TSUg <enriquearana1402@gmail.com>
2025-04-04 13:17:54 -0500

    Semana 02

---
+++ Desarrollo/Assets/Personajes/Ninja/Scrips/PlayerControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocityX = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.linearVelocityX = 5;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.linearVelocityX = -5;
+        }
+    }
+}
